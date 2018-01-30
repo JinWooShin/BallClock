11e5eadb0a9647ff477f597823c452a22205d97d
Jin Woo Shin <cootang2@gmail.com>
2018-01-29 21:30:32 -0700

    add bootstap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
 import { BallClockComponent } from './ball-clock/ball-clock.component';
 
@@ -16,7 +15,11 @@ import { BallClockComponent } from './ball-clock/ball-clock.component';
     BrowserModule,
     FormsModule
   ],
+  exports: [
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
